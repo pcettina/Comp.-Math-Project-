fd649420ef2dab82742c934f31604a3e4c884152
ethsen <ethansenatore@gmail.com>
2024-04-23 13:02:16 -0400

    Update on 04/23/2024

--- roboArmModel.py
+++ roboArmModel.py
@@ -36,27 +36,84 @@ def vectorCalc(a1,a2):
     r = np.dot(r,np.vstack([0,0,1]))
     return r
 
+def angle(theta, phi):
+    arm1_1 = [np.cos(theta), -np.sin(theta), np.cos(theta)]
+    arm1_2 = [np.sin(theta), np.cos(theta), np.sin(theta)]
+    arm1_3 = [0,0,1]
+
+    arm1 = np.array([arm1_1, arm1_2, arm1_3])
+
+    arm2_1 = [np.cos(phi), -np.sin(phi), np.cos(phi)]
+    arm2_2 = [np.sin(phi), np.cos(phi), np.sin(phi)]
+    arm2_3 = [0,0,1]
+
+    arm2 = np.array([arm2_1, arm2_2, arm2_3])
+
+    return arm1, arm2
 
 def main():
     l1 = 1
     l2 = 1
 
     
+    arm1, arm2 = angle(np.deg2rad(30.651), np.deg2rad(51.317))
+
+    r = vectorCalc(arm1,arm2)
+    
+
     pos1 = [1,1.5]
-    pos2 = [-1 ,1.5]
 
     s1,c1,s2,c2 = coefCalc(pos1[0] ,pos1[1])
-    a1 = mapper(s1,c1,l1)
-    a2 = mapper(s2,c2,l2)
-    r = vectorCalc(a1,a2)
+    initTheta = np.arccos(c1)
+    initPhi = np.arccos(c2)
+    
+
+    pos2 = [-1,1.5]
+    s1,c1,s2,c2 = coefCalc(pos2[0] ,pos2[1])
+    finalTheta = np.arccos(c1)
+    finalPhi = np.arccos(c2)
+    
+    #a1 = mapper(s1,c1,l1)
+    #a2 = mapper(s2,c2,l2)
+   # r = vectorCalc(a1,a2)
 
     fig = plt.figure()
     plot = fig.add_subplot(111)
 
-    plot.plot([0, c1],[0, s1],'r')
-    plot.plot([c1,r[0][0]],[s1,r[1][0]], 'b-')
-    plot.set_xlim([-1,1])
-    plot.set_ylim([0,2])
+    #plot.plot([0, np.cos(np.radians(30.651))],[0, np.sin(np.radians(30.651))],'r')
+    #plot.plot([np.cos(np.radians(30.651)),r[0][0]],[np.sin(np.radians(30.651)),r[1][0]], 'b-')
+
+    
+
+
+    theta = np.linspace(initTheta,finalTheta,10)
+    phi = np.linspace(initPhi, finalPhi,10)
+
+    
+    for thetas,phis in zip(theta,phi):
+        arm1, arm2 =  angle(thetas,phis)
+        r = vectorCalc(arm1,arm2)
+        plot.plot([0, np.cos(thetas)],[0, np.sin(thetas)],'r')
+        plot.plot([np.cos(thetas),r[0][0]],[np.sin(thetas),r[1][0]], 'b-')
+
+
+    arm1, arm2 =  angle(finalTheta,finalPhi)
+    r = vectorCalc(arm1,arm2)
+    plot.plot([0, np.cos(finalTheta)],[0, np.sin(finalTheta)],'k')
+    plot.plot([np.cos(finalTheta),r[0][0]],[np.sin(finalTheta),r[1][0]], 'k-')
+
+    #s1,c1,s2,c2 = coefCalc(pos2[0] ,pos2[1])
+    #print(np.degrees(np.arccos(c2)))
+    
+    #a1 = mapper(s1,c1,l1)
+    #a2 = mapper(s2,c2,l2)
+    #r = vectorCalc(a1,a2)
+
+    #plot.plot([0, c1],[0, s1],'r')
+    #plot.plot([c1,r[0][0]],[s1,r[1][0]], 'b-')
+
+    plot.set_xlim([-5,5])
+    plot.set_ylim([-5,5])
 
     
     plt.show()
